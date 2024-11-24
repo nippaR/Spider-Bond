0112ef124fda619b0f7f530122ca89c53d4a122f
Thanuja Nipun <thanujanipun6@gmail.com>
2024-11-25 03:41:28 +0530

    Add Home Page to Header Section, and modifiying home page content

--- spidy/src/App.js
+++ spidy/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './Frontend/HomePage';
+import Header from './Frontend/Header';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path='/header' element={<Header />} />
       </Routes>
     </Router>
   );
--- spidy/src/Frontend/HomePage.js
+++ spidy/src/Frontend/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Typography, Box, Button } from '@mui/material';
+import {Box, Grid, Typography,Button,Stack} from '@mui/material';
 import backgroundImage from '../img/wp14198855.webp'; // Import the image
+import Header from './Header';
 
 const HomePage = () => {
   return (
+  <Grid>
     <Box
       sx={{
         minHeight: '100vh', // Ensure it covers the full viewport height
@@ -11,23 +13,70 @@ const HomePage = () => {
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
-        display: 'flex',
+        display: 'block',
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
-        textAlign: 'center',
+        // textAlign: 'center',
         color: 'white',
-        opacity: 0.9, // Add some transparency
       }}
     >
-      <Typography variant="h2" gutterBottom>
-        Welcome to Spidy Bond
+      <Header />
+     
+    <Stack spacing={12} direction="row" sx={{ml:7, my:5, gap:30 }}>
+     <Grid item xs={12} sm={6}> 
+     <Typography variant="h1" sx={{ my:5, fontFamily: 'poppins', fontWeight: 400, letterSpacing: '.5rem', fontSize: '4.5rem' }}>
+       Spidey Bond
       </Typography>
-      <Button size='50%'>OnlyFans</Button>
-      <Typography variant="body1" fontSize="50px">
-       Only Fans
-      </Typography>
+
+            <Typography variant="h4" sx={{my:5, fontFamily: 'poppins', fontWeight: 100, fontSize: '1rem' }}>
+                He said the idea for Spider-Man arose from a surge in teenage  {<br />}
+                demand for comic books and the desire to create a character {<br />}
+                with whom teens could identify. As with Fantastic Four, Lee saw {<br />}
+                Spider-Man as an opportunity to "get out of his system" what he {<br />}
+                felt was missing in comic books.
+            </Typography>
+      
+              <Button variant="contained"
+              sx={{my:5, 
+                    fontFamily: 'poppins', 
+                    fontWeight: 400, 
+                    fontSize: '1rem',
+                    backgroundColor:'#831005',
+                    '&:hover':{color:'white'},
+                    borderRadius:'10px' }}>
+                  
+                  Only Fans
+
+              </Button>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+      <Box  sx={{
+            width: '400px', // Set the width of the rectangle
+            height: '480px', // Set the height of the rectangle
+            backgroundColor: 'black', // Set the background color
+            opacity: 0.6, // Set the opacity
+            borderRadius: '80px', // Optional: add border radius for rounded corners
+            boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)', // Add shadow for depth
+          }}>
+      </Box>
+      <Button variant="contained" 
+        sx={{fontFamily: 'poppins',
+             fontWeight: 400, 
+             fontSize: '1rem', 
+             backgroundColor:'black', 
+             '&:hover':{color:'white'},
+             borderRadius: '10px',
+             my:1,
+             ml:19,
+              }}>
+          Take Me
+        </Button>
+      </Grid>
+
+      </Stack>
     </Box>
+  </Grid>
   );
 };
 
