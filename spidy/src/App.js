a6a9aaee852de2e8ceef1475efd1e80bef397663
Thanuja Nipun <thanujanipun6@gmail.com>
2024-12-16 07:00:16 +0530

    Adding a signin page

--- spidy/src/App.js
+++ spidy/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import HomePage from './Frontend/HomePage';
 import Header from './Frontend/Header';
 import TakeMe from './Frontend/TakeMe';
@@ -8,11 +8,26 @@ import Section from './Frontend/Sections';
 import Footer from './Frontend/Footer';
 import Products from './Frontend/Products';
 import Card from './Frontend/Card';
+import Signin from './Frontend/Signin';
 
 function App() {
   return (
     <Router>
-      <Header />
+      <MainContent />
+    </Router>
+  );
+}
+
+function MainContent() {
+  const location = useLocation();
+
+  // Determine if the current path is '/signin'
+  const isSigninPage = location.pathname === '/signin';
+
+  return (
+    <>
+      {/* Conditionally render Header and Footer */}
+      {!isSigninPage && <Header />}
       <Routes>
         <Route path="/home" element={<HomePage />} />
         <Route path='/takeme' element={<TakeMe />} />
@@ -20,9 +35,10 @@ function App() {
         <Route path='/' element={<Section/>} />
         <Route path='/products' element={<Products/>} />
         <Route path='/card' element={<Card/>} />
+        <Route path='/signin' element={<Signin />} />
       </Routes>
-      <Footer />
-    </Router>
+      {!isSigninPage && <Footer />}
+    </>
   );
 }
 
